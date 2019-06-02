b4da4dfe02c18ed7254ded28365463d3825ede03
oksana-d <oksana.yegorova99@gmail.com>
2019-06-02 14:10:55 +0300

    Added class to work with YouTube API

---
+++ src/application/YouTubeVideo.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace src\application;
+use Google_Client;
+use Google_Service_YouTube;
+use Google_Service_Exception;
+
+class YouTubeVideo
+{
+
+    public $id;
+    private $apiKey = 'AIzaSyCnRk-TqMSWECmNWEjMJm1CmL7Lnozh5EY';
+    private $youtube;
+
+    public function __construct()
+    {
+        $client = new Google_Client();
+        $client->setDeveloperKey($this->apiKey);
+        $this->youtube = new Google_Service_YouTube($client);
+    }
+
+    public function search(string $query )//Поиск видео по фразе
+    {
+        $maxResults=12;
+        $lang='ru';
+
+        $response = $this->youtube->search->listSearch('snippet',
+            array(
+                'q' => $query,
+                'maxResults' => $maxResults,
+                'relevanceLanguage' => $lang,
+                'type' => 'video'
+            ));
+
+        return $response;
+    }
+
+    public  function nextPage($query ,$nextPageToken){
+        $maxResults=12;
+        $lang='ru';
+
+        $response = $this->youtube->search->listSearch('snippet',
+            array(
+                'q' => $query,
+                'maxResults' => $maxResults,
+                'relevanceLanguage' => $lang,
+                'type' => 'video',
+                'pageToken' => $nextPageToken
+            ));
+
+        return $response;
+    }
+}
